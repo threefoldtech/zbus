daa62f4d24c6aae307dc184e0adf78329e7f94c4
Muhamad Azmy <muhamada@greenitglobe.com>
2019-05-19 09:31:51 +0200

    Apply review suggestion
    Co-Authored-By: Christophe de Carvalho <zaibon@users.noreply.github.com>

--- redis.go
+++ redis.go
@@ -276,7 +276,11 @@ func (c *RedisClient) Stream(ctx context.Context, module string, object ObjectID
 
 	ch := make(chan Event)
 	go func(con redis.Conn) {
-		defer con.Close()
+		defer func (){
+		    close(ch)
+		    con.Send("UNSUBSCRIBE")
+		    con.Close()		    
+		 }()
 		defer con.Send("UNSUBSCRIBE")
 		defer close(ch)
 
